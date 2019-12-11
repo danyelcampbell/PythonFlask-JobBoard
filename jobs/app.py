e4e4a2120df83f459528e0ce607cdf6e16613e14
danyel.campbell <danyel.campbell@gmail.com>
2019-12-11 14:51:50 -0600

    added close_connection function, edited open_connection and execute_sql functions

--- jobs/app.py
+++ jobs/app.py
@@ -1,24 +1,30 @@
-###Danyel Campbell
-###December 9, 2019
-###Job Board Web Application
+"""
+Name: Danyel Campbell
+Date: December 9, 2019
+Title: Job Board Web Application
+Purpose: TBD :D
+"""
 
 
 from flask import Flask, render_template, g
 import sqlite3
 
-PATH = "db/jobs.sqlite3"
+PATH = "db/jobs.sqlite"
 
-app = Flask(__name__) #creates an instance of Flask called app with variable __name__
+app = Flask(__name__) # creates an instance of Flask called app with variable __name__
 
-def open_connection(): #connects to the specific database
-    def getattr(g, '_connection', None):
-        """returns the value of the _connection attribute of object g,
-         if it's not found it will return None"""
-        return _connection
-    if _connection == None:
-        _connection = g._connection = sqlite.connection(PATH)
-    row_factory = sqlite3.Row
-    return _connection
+def open_connection(): # connects to the specific database
+    connection = getattr(g, '_connection', None) #returns the value of the _connection attribute of object g, if it's not found it will return None"""
+    if connection == None:
+        connection = g._connection = sqlite.connection(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+@app.teardown_appcontext
+def close_connection(exception): # disconnects the database connection when app_context is torn down
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+            connection.close()
 
 def execute_sql(sql, values, commit, single):
     connection = open_connection()
@@ -26,15 +32,13 @@ def execute_sql(sql, values, commit, single):
     commit = False
     single = False
     cursor = connection.execute(sql, values)
-    return cursor
     if commit == True:
         results = connection.commit()
-    else:
-        if curs
+    results = cursor.fetchone() if single else cursor.fetchall() # if a single result, fetch one, else fetch all
+    return results
 
 
-
-#will tell Flask what URL to trigger the function
+# will tell Flask what URL to trigger the function
 @app.route("/")
 @app.route("/jobs")
 def jobs():
